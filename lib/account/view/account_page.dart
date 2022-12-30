19e4b931df9f2d5de528be677327f8e6264b6224
Yosaire <96118628+Yosaire@users.noreply.github.com>
2022-12-30 15:04:35 +0100

    Added showDataPicker and field to change password

--- lib/account/view/account_page.dart
+++ lib/account/view/account_page.dart
@@ -2,8 +2,11 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-String name = "IgorPrawo";
+String name = "";
+DateTime dateAccount = DateTime(2022, 12, 30);
+
 TextEditingController nameController = TextEditingController();
+TextEditingController passwordController = TextEditingController();
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -225,10 +228,24 @@ class _AccountPageState extends State<AccountPage> {
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                   Expanded(child: SizedBox()),
-                  const Text(
-                    'DataPrawo',
-                    style: TextStyle(
-                      color: Colors.grey,
+                  GestureDetector(
+                    onTap: () async {
+                      DateTime? newDate = await showDatePicker(
+                        context: context,
+                        initialDate: dateAccount,
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime(2023),
+                      );
+                      if (newDate == null) return;
+                      setState(() {
+                        dateAccount = newDate;
+                      });
+                    },
+                    child: Text(
+                      '${dateAccount.year}.${dateAccount.month}.${dateAccount.day}',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
                     ),
                   ),
                   const Icon(
@@ -248,10 +265,123 @@ class _AccountPageState extends State<AccountPage> {
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                   Expanded(child: SizedBox()),
-                  const Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.grey,
-                    size: 18,
+                  GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                          alignment: Alignment.bottomCenter,
+                          child: Stack(
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                                height: 280,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Zmien imię",
+                                      textAlign: TextAlign.center,
+                                      style:
+                                          TextStyle(fontSize: 18, height: 1.5),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.fromLTRB(10, 10, 10, 10),
+                                    ),
+                                    SizedBox(
+                                      width: double.infinity,
+                                      height: 50,
+                                      child: TextField(
+                                        decoration: InputDecoration(
+                                          filled: true,
+                                          fillColor: Color.fromARGB(
+                                              255, 250, 250, 250),
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                          ),
+                                          hintText: 'Podaj stare hasło',
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 20),
+                                    SizedBox(
+                                      width: double.infinity,
+                                      height: 50,
+                                      child: TextField(
+                                        controller: passwordController,
+                                        decoration: InputDecoration(
+                                          filled: true,
+                                          fillColor: Color.fromARGB(
+                                              255, 250, 250, 250),
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                          ),
+                                          hintText: 'Podaj nowe hasło',
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 20),
+                                    Row(
+                                      children: [
+                                        SizedBox(
+                                          width: 120,
+                                          child: ElevatedButton(
+                                            onPressed: () =>
+                                                Navigator.pop(context),
+                                            child: Text(
+                                              "Anuluj",
+                                              textAlign: TextAlign.left,
+                                            ),
+                                            style: ElevatedButton.styleFrom(
+                                                backgroundColor: Color.fromARGB(
+                                                    255, 250, 250, 250),
+                                                foregroundColor: Colors.black,
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(15),
+                                                )),
+                                          ),
+                                        ),
+                                        Expanded(child: SizedBox()),
+                                        SizedBox(
+                                          width: 120,
+                                          child: ElevatedButton(
+                                            onPressed: () => {
+                                              Navigator.pop(context),
+                                              changeName()
+                                            },
+                                            child: Text(
+                                              "Ok",
+                                              textAlign: TextAlign.left,
+                                            ),
+                                            style: ElevatedButton.styleFrom(
+                                                backgroundColor:
+                                                    Colors.lightBlue,
+                                                foregroundColor: Colors.white,
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(15),
+                                                )),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.grey,
+                      size: 18,
+                    ),
                   ),
                 ],
               )
